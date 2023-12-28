5aaa376fd9a916186ccec520ee26b7796d44a959
antonioccunha <antonio.cacao.cunha00@gmail.com>
2023-12-28 14:51:40 +0000

    Revert "Flight menu + some bugs"
    This reverts commit fd50ffb354f62ac43114f6dcc6c89c562cc19312.

--- src/FMSGraph.h
+++ src/FMSGraph.h
@@ -44,11 +44,13 @@ public:
     void connectedComponentsDfsVisit(Vertex<Airport>* v, set<std::string> & airportCount);
     int lowestNumberOfStops(Vertex<Airport>* source, Vertex<Airport>* destination);
     set<vector<Vertex<Airport>*>> findAllShortestPathsBetweenAirports(Vertex<Airport>* source, Vertex<Airport>* destination);
-    void bestFlightOption(Vertex<Airport>* source , Vertex<Airport>* destination);
+    void bestFlightOption();
     vector<Airport> cityAirports(std::string city);
     Vertex<Airport>* cityOption();
     Vertex<Airport>* airportOption();
     Vertex<Airport>* coordinatesOption();
+    vector<Airline> getSelectedAirlines();
+    FMSGraph airlineFilter(vector<Airline> selectedAirlines);
 };
 
 

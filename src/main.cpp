a99c65ea218be32ad2b4fb1d8407e3974696b337
João Parada <up201405280@edu.fe.up.pt>
2024-01-02 18:08:10 +0000

    fixed file path

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,17 @@
 //
 #include "FileReader.h"
 #include <iostream>
+#include <filesystem>
 #include "Menu.h"
 #include "FMSGraph.h"
 
 int main(){
     std::cout << "Hello World :D" << std::endl;
+    std::cout << "Current Working Directory: " << std::filesystem::current_path() << std::endl;
     FMSGraph globalGraph;
-    FileReader::airlineMap("data/airlines.csv",globalGraph);
-    FileReader::addAirports("data/airports.csv", globalGraph);
-    FileReader::addFlights("data/flights.csv",globalGraph);
+    FileReader::airlineMap("../data/airlines.csv",globalGraph);
+    FileReader::addAirports("../data/airports.csv", globalGraph);
+    FileReader::addFlights("../data/flights.csv",globalGraph);
     Menu menu(globalGraph);
     menu.getMainMenu(globalGraph);
     return 0;
